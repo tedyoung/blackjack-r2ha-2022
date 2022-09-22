d439a45cc76a8b07a605e542bb04903f7be9c410
Ted M. Young <tedyoung@gmail.com>
2022-09-21 19:17:44 -0700

    LAB #1: Added characterization test for Card with rank of TEN

---
+++ src/test/java/com/r2ha/blackjack/CardDisplayTest.java
@@ -0,0 +1,15 @@
+package com.r2ha.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CardDisplayTest {
+    @Test
+    void displayTenAsString() {
+        Card card = new Card(Suit.CLUBS, Rank.TEN);
+
+        assertThat(card.display())
+                .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+}
