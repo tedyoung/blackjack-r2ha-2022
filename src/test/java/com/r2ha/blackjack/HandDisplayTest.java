a66c8e0ed1f547ec3eb71b1c6ef1778f91001d52
Ted M. Young <tedyoung@gmail.com>
2022-09-29 15:10:50 -0700

    Lab #2: Add passing characterization test for displayFaceUpCard.

---
+++ src/test/java/com/r2ha/blackjack/HandDisplayTest.java
@@ -0,0 +1,22 @@
+package com.r2ha.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HandDisplayTest {
+
+    @Test
+    void displayFaceUpCardReturnsAsCorrectString() throws Exception {
+        List<Card> cards = List.of(
+                new Card(Suit.HEARTS, Rank.ACE),
+                new Card(Suit.CLUBS, Rank.JACK));
+        Hand hand = new Hand(cards);
+
+        assertThat(hand.displayFaceUpCard())
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
+    }
+
+}
