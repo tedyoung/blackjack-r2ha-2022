fb2c0b5f2aa7676a660d5bf6e98a675f2348b7a1
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-10-22 20:53:34 +0700

    test(Lab 2): add cardsAsStringTranslatesCardsToString test

--- src/test/java/com/r2ha/blackjack/HandDisplayTest.java
+++ src/test/java/com/r2ha/blackjack/HandDisplayTest.java
@@ -19,4 +19,16 @@ public class HandDisplayTest {
                 .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
     }
 
+    @Test
+    public void cardsAsStringTranslatesCardsToString() throws Exception {
+        Hand hand = new Hand(
+                List.of(
+                        new Card(Suit.CLUBS, Rank.QUEEN),
+                        new Card(Suit.HEARTS, Rank.KING)
+                        )
+        );
+
+        assertThat(hand.cardsAsString())
+                .isEqualTo("\u001B[30m┌─────────┐\u001B[1B\u001B[11D│Q        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        Q│\u001B[1B\u001B[11D└─────────┘\u001B[6A\u001B[1C\u001B[31m┌─────────┐\u001B[1B\u001B[11D│K        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        K│\u001B[1B\u001B[11D└─────────┘");
+    }
 }
